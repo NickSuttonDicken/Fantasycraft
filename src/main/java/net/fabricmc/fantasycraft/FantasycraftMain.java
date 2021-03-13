d48d6864b43e15925f9866e1b4f3ade1e72db9b7
Frozti <nicholassuttondicken@gmail.com>
2021-03-14 00:19:35 +1300

    Added Dwarven Blast Furnace

--- src/main/java/net/fabricmc/fantasycraft/FantasycraftMain.java
+++ src/main/java/net/fabricmc/fantasycraft/FantasycraftMain.java
@@ -6,18 +6,25 @@ import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
-import net.fabricmc.fabric.impl.biome.modification.BiomeModificationContextImpl;
 import net.fabricmc.fantasycraft.armor.MithrilArmorMaterial;
+import net.fabricmc.fantasycraft.blocks.DwarvenBlastFurnace;
 import net.fabricmc.fantasycraft.blocks.MithrilBlock;
 import net.fabricmc.fantasycraft.blocks.MithrilOre;
+import net.fabricmc.fantasycraft.entity.DwarvenBlastFurnaceEntity;
 import net.fabricmc.fantasycraft.items.MithrilIngot;
+import net.fabricmc.fantasycraft.recipe.DwarvenBlasting;
+import net.fabricmc.fantasycraft.client.DwarvenBlastFurnaceScreenHandler;
 import net.fabricmc.fantasycraft.tools.*;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
-import net.minecraft.util.Arm;
+import net.minecraft.recipe.CookingRecipeSerializer;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
@@ -28,11 +35,12 @@ import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
-import org.lwjgl.system.CallbackI;
-
-import javax.tools.Tool;
 
 public class FantasycraftMain implements ModInitializer {
+
+	//Mod Details
+	public static final String MOD_ID = "fantasycraft";
+	public static final String MOD_NAME = "Fantasycraft";
 
 	//Item Group
 	public static final ItemGroup Fantasycraft = FabricItemGroupBuilder.build(
@@ -43,6 +51,7 @@ public class FantasycraftMain implements ModInitializer {
 	public static final MithrilIngot MITHRIL_INGOT = new MithrilIngot(new Item.Settings().group(FantasycraftMain.Fantasycraft));
 
 	//Blocks
+	public static final DwarvenBlastFurnace DWARVEN_BLAST_FURNACE = new DwarvenBlastFurnace(FabricBlockSettings.of(Material.METAL));
 	public static final MithrilBlock MITHRIL_BLOCK = new MithrilBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f).breakByTool(FabricToolTags.PICKAXES, 4).requiresTool());
 	public static final MithrilOre MITHRIL_ORE = new MithrilOre(FabricBlockSettings.of(Material.STONE).breakByHand(false).strength(4.0f).breakByTool(FabricToolTags.PICKAXES, 4).requiresTool());
 
@@ -60,7 +69,21 @@ public class FantasycraftMain implements ModInitializer {
 	public static final Item MITHRIL_LEGGINGS = new ArmorItem(MITHRIL_ARMOR_MATERIAL, EquipmentSlot.LEGS, new Item.Settings().group(FantasycraftMain.Fantasycraft));
 	public static final Item MITHRIL_BOOTS = new ArmorItem(MITHRIL_ARMOR_MATERIAL, EquipmentSlot.FEET, new Item.Settings().group(FantasycraftMain.Fantasycraft));
 
-	//Mithril Ore Generation
+	//Recipe
+	public static final RecipeType<DwarvenBlasting> DWARVEN_BLASTING_RECIPE_TYPE = new RecipeType<DwarvenBlasting>() {
+		@Override
+		public String toString() {
+			return "dwarven_blast_furnace"; }
+	};
+	public static final RecipeSerializer<DwarvenBlasting> DWARVEN_BLASTING_RECIPE_SERIALIZER = new CookingRecipeSerializer<>(DwarvenBlasting::new, 50);
+
+	//Client
+	public static final ScreenHandlerType<DwarvenBlastFurnaceScreenHandler> DWARVEN_BLAST_FURNACE_SCREEN_HANDLER;
+
+	//Entities
+	public static final BlockEntityType DWARVEN_BLAST_FURNACE_ENTITY = BlockEntityType.Builder.create(DwarvenBlastFurnaceEntity::new, DWARVEN_BLAST_FURNACE).build(null);
+
+	//Ore Generation
 	private static ConfiguredFeature<?,?> ORE_MITHRIL_OVERWORLD = Feature.ORE
 			.configure(new OreFeatureConfig(
 					OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
@@ -75,34 +98,48 @@ public class FantasycraftMain implements ModInitializer {
 			.repeat(2);
 
 
+	static {
+		//Client Registration
+		DWARVEN_BLAST_FURNACE_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(new Identifier(MOD_ID, "dwarven_blast_furnace"), DwarvenBlastFurnaceScreenHandler::new);
+	}
 
 	@Override
 	public void onInitialize() {
 		//Blocks Registration
-		Registry.register(Registry.BLOCK, new Identifier("fantasycraft", "mithril_ore"), MITHRIL_ORE);
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_ore"), new BlockItem(MITHRIL_ORE, new FabricItemSettings().group(FantasycraftMain.Fantasycraft)));
-		Registry.register(Registry.BLOCK, new Identifier("fantasycraft", "mithril_block"), MITHRIL_BLOCK);
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_block"), new BlockItem(MITHRIL_BLOCK, new FabricItemSettings().group(FantasycraftMain.Fantasycraft)));
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "mithril_ore"), MITHRIL_ORE);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "mithril_block"), MITHRIL_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "dwarven_blast_furnace"), DWARVEN_BLAST_FURNACE);
 
 		//Items Registration
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_ingot"), MITHRIL_INGOT);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_ingot"), MITHRIL_INGOT);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_block"), new BlockItem(MITHRIL_BLOCK, new FabricItemSettings().group(FantasycraftMain.Fantasycraft)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_ore"), new BlockItem(MITHRIL_ORE, new FabricItemSettings().group(FantasycraftMain.Fantasycraft)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "dwarven_blast_furnace"), new BlockItem(DWARVEN_BLAST_FURNACE, new FabricItemSettings().group(FantasycraftMain.Fantasycraft)));
 
 		//Tools Registration
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_pickaxe"), MITHRIL_PICKAXE);
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_shovel"), MITHRIL_SHOVEL);
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_axe"), MITHRIL_AXE);
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_hoe"), MITHRIL_HOE);
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_sword"), MITHRIL_SWORD);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_pickaxe"), MITHRIL_PICKAXE);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_shovel"), MITHRIL_SHOVEL);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_axe"), MITHRIL_AXE);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_hoe"), MITHRIL_HOE);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_sword"), MITHRIL_SWORD);
 
 		//Armor Registration
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_helmet"), MITHRIL_HELMET);
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_chestplate"), MITHRIL_CHESTPLATE);
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_leggings"), MITHRIL_LEGGINGS);
-		Registry.register(Registry.ITEM, new Identifier("fantasycraft", "mithril_boots"), MITHRIL_BOOTS);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_helmet"), MITHRIL_HELMET);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_chestplate"), MITHRIL_CHESTPLATE);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_leggings"), MITHRIL_LEGGINGS);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mithril_boots"), MITHRIL_BOOTS);
+
+		//Recipe Registration
+		Registry.register(Registry.RECIPE_TYPE, new Identifier(MOD_ID, "dwarven_blast_furnace"), DWARVEN_BLASTING_RECIPE_TYPE);
+		Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(MOD_ID, "dwarven_blast_furnace"), DWARVEN_BLASTING_RECIPE_SERIALIZER);
+
+
+		//Entities Registration
+		Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier(MOD_ID, "dwarven_blast_furnace"), DWARVEN_BLAST_FURNACE_ENTITY);
 
 		//World Generation Registration
 		RegistryKey<ConfiguredFeature<?,?>> oreMithrilOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN,
-				new Identifier("fantasycraft", "ore_mithril_overworld"));
+				new Identifier(MOD_ID, "ore_mithril_overworld"));
 		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreMithrilOverworld.getValue(), ORE_MITHRIL_OVERWORLD);
 		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreMithrilOverworld);
 	}
